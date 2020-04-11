0a1e7da341df4e0b88fb653bb26f98ec3355f537
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2020-04-11 11:09:50 -0400

    Added a spinner on the home page while data is loading

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import axios from 'axios';
-import CatchesGrid from './CatchesGrid';
-import API from '../API'
+import React from "react";
+import axios from "axios";
+import CatchesGrid from "./CatchesGrid";
+import API from "../API";
 
 function Home() {
-    const [getCatches, setCatches] = React.useState([]);
+  const [getCatches, setCatches] = React.useState([]);
+  const [getLoading, setLoading] = React.useState(true);
 
-    React.useEffect(() => {
-        // axios.get('https://us-central1-hogsnatcher-3e6c3.cloudfunctions.net/api/catches')
-        // .then((res) => {
-        //     setCatches(res.data);
-        // }).catch(err => {
-        //     console.error(err);
-        // });
-        API.getCatches().then(data => {
-            setCatches(data);
-        })
-    })
+  React.useEffect(() => {
+    // axios.get('https://us-central1-hogsnatcher-3e6c3.cloudfunctions.net/api/catches')
+    // .then((res) => {
+    //     setCatches(res.data);
+    // }).catch(err => {
+    //     console.error(err);
+    // });
+    API.getCatches().then((data) => {
+      setLoading(false);
+      setCatches(data);
+    });
+  });
 
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <CatchesGrid catches={getCatches}></CatchesGrid>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <CatchesGrid catches={getCatches}></CatchesGrid>
+      <div className="text-center">
+        {getLoading && (
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
